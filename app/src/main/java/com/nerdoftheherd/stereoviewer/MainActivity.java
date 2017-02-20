3d6e533c0b632a4c541a37265a362baa406a5080
Matt Robinson <git@nerdoftheherd.com>
2017-02-20 20:47:59 +0000

    Add ViewActivity from fullscreen activity template
    Create a new ViewActivity from the Fullscreen activity template, with
    the compatability library and references to it stripped out.

--- app/src/main/java/com/nerdoftheherd/stereoviewer/MainActivity.java
+++ app/src/main/java/com/nerdoftheherd/stereoviewer/MainActivity.java
@@ -19,12 +19,19 @@
 package com.nerdoftheherd.stereoviewer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewImage(View view) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
     }
 }
