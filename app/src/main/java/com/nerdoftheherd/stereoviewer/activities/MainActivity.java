cb8439e13ddd39f237bf526f233c3e12cad2a952
Matt Robinson <git@nerdoftheherd.com>
2017-02-26 17:13:42 +0000

    Allow viewing images within a folder
    Add the option of selecting a whole folder of images at once and
    navigating through them with left and right arrows on screen (made
    invisible at the same time as the rest of the viewing UI, but can still
    be clicked).

--- app/src/main/java/com/nerdoftheherd/stereoviewer/activities/MainActivity.java
+++ app/src/main/java/com/nerdoftheherd/stereoviewer/activities/MainActivity.java
@@ -19,14 +19,21 @@
 package com.nerdoftheherd.stereoviewer.activities;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.View;
 
 import com.nerdoftheherd.stereoviewer.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
     private static final int DOCUMENT_PICKED = 1;
+    private static final int DOCUMENT_TREE_PICKED = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +41,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void selectFolder(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, DOCUMENT_TREE_PICKED);
+    }
+
     public void selectFile(View view) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
+        intent.setType("image/jpeg");
         startActivityForResult(intent, DOCUMENT_PICKED);
     }
 
@@ -48,5 +60,34 @@ public class MainActivity extends Activity {
             intent.putExtra(ViewActivity.INTENT_DATA_IMAGEURI, data.getData());
             startActivity(intent);
         }
+        else if (requestCode == DOCUMENT_TREE_PICKED && resultCode == RESULT_OK) {
+            findChildren(data.getData());
+        }
+    }
+
+    private void findChildren(Uri uri) {
+        ContentResolver contentResolver = this.getContentResolver();
+        Uri childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(uri, DocumentsContract.getTreeDocumentId(uri));
+
+        ArrayList<Uri> uris = new ArrayList<Uri>();
+
+        try (Cursor childCursor = contentResolver.query(
+                childrenUri,
+                new String[]{
+                        DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+                        DocumentsContract.Document.COLUMN_MIME_TYPE
+                },
+                null, null, null)) {
+            while (childCursor.moveToNext()) {
+                if(childCursor.getString(1).equals("image/jpeg")) {
+                    Uri childUri = DocumentsContract.buildDocumentUriUsingTree(uri, childCursor.getString(0));
+                    uris.add(childUri);
+                }
+            }
+        }
+
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra(ViewActivity.INTENT_DATA_IMAGEURIS, uris);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/nerdoftheherd/stereoviewer/activities/ViewActivity.java
+++ app/src/main/java/com/nerdoftheherd/stereoviewer/activities/ViewActivity.java
@@ -24,17 +24,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.nerdoftheherd.stereoviewer.R;
 import com.nerdoftheherd.stereoviewer.image.SideBySideImage;
 
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
 
 public class ViewActivity extends Activity {
     public static final String INTENT_DATA_IMAGEURI = "imageuri";
+    public static final String INTENT_DATA_IMAGEURIS = "imageuris";
 
     private final Handler mHideHandler = new Handler();
     private final Runnable mHideRunnable = new Runnable() {
@@ -43,11 +46,16 @@ public class ViewActivity extends Activity {
             hide();
         }
     };
-    private boolean mVisible;
+
+    private boolean mVisible = true;
+    private int mImageIndex = 0;
+    private List<Uri> mImageUris;
 
     private View mContentView;
     private ImageView mLeftImage;
     private ImageView mRightImage;
+    private ImageView mPrevious;
+    private ImageView mNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,24 +67,19 @@ public class ViewActivity extends Activity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        mVisible = true;
         mContentView = findViewById(R.id.fullscreen_content);
         mLeftImage = (ImageView)findViewById(R.id.left_image);
         mRightImage = (ImageView)findViewById(R.id.right_image);
+        mPrevious = (ImageView)findViewById(R.id.showPrevious);
+        mNext = (ImageView)findViewById(R.id.showNext);
 
-        Uri fileUri = getIntent().getParcelableExtra(INTENT_DATA_IMAGEURI);
-        SideBySideImage img;
+        mImageUris = getIntent().getParcelableArrayListExtra(INTENT_DATA_IMAGEURIS);
 
-        try {
-            img = new SideBySideImage(fileUri, this.getContentResolver());
-        }
-        catch(FileNotFoundException exp) {
-            // Just crash the app for the time being
-            throw new RuntimeException(exp);
+        if(mImageUris == null) {
+            mImageUris = Collections.singletonList((Uri)getIntent().getParcelableExtra(INTENT_DATA_IMAGEURI));
         }
 
-        mLeftImage.setImageBitmap(img.LeftImage());
-        mRightImage.setImageBitmap(img.RightImage());
+        loadImage(mImageUris.get(mImageIndex));
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -125,6 +128,8 @@ public class ViewActivity extends Activity {
         }
 
         mVisible = false;
+        mPrevious.setAlpha(0f);
+        mNext.setAlpha(0f);
 
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -132,16 +137,49 @@ public class ViewActivity extends Activity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
     }
 
     private void show() {
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
         mVisible = true;
+        mPrevious.setAlpha(1f);
+        mNext.setAlpha(1f);
 
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             actionBar.show();
         }
+    }
+
+    public void showNext(View view) {
+        if(mImageIndex + 1 < mImageUris.size())
+        {
+            loadImage(mImageUris.get(++mImageIndex));
+        }
+    }
+
+    public void showPrevious(View view) {
+        if(mImageIndex > 0)
+        {
+            loadImage(mImageUris.get(--mImageIndex));
+        }
+    }
+
+    private void loadImage(Uri uri) {
+        SideBySideImage img;
+
+        try {
+            img = new SideBySideImage(uri, this.getContentResolver());
+        }
+        catch(FileNotFoundException exp) {
+            // Just crash the app for the time being
+            throw new RuntimeException(exp);
+        }
+
+        mLeftImage.setImageBitmap(img.LeftImage());
+        mRightImage.setImageBitmap(img.RightImage());
     }
 }
